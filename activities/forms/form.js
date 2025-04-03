c67616a66e9083ad6ea5c620fc396660a39e953f
Louis Naingue <louis.naingue@gmail.com>
2025-04-02 15:41:36 -1000

    add changes

--- activities/forms/form.js
+++ activities/forms/form.js
@@ -3,22 +3,10 @@ document.getElementById('myForm').addEventListener('submit',function(event) {
     //alert("Form Submitted");
     const fullname = document.getElementById('fname').value;
     const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const age = document.getElementById('age').value;
-
-    if(!fullanme || lastname){
-        alert("You need a firts name and lastname")
-        retur;
-    }
-    if(!age || age <18){
-        alert("You must be 18 years old")
-        return;
-    }
+   
    const formData = {
         name: fullname,
         email: email,
-        password: password,
-        age: age,
 
    }
     console.log(fullname);
